4f90dac5705de2c08a254938adda29c389895859
Mike Hall <mcsyko@gmail.com>
2015-10-02 10:05:43 +0200

    cleanup

--- ImageQuality.OpenCV/IntensityMeasure.cpp
+++ ImageQuality.OpenCV/IntensityMeasure.cpp
@@ -9,17 +9,7 @@ namespace ImageQuality
 	double IntensityMeasure::IntensityTest(array<byte>^ buffer)
 	{
 		Mat image = ReadImage(buffer);
-		/*Mat hist;
-
-		int channels[] = { 0 };
-		int histSize[] = { 32 };
-		float range[] = { 0, 256 };
-		const float* ranges[] = { range };
-
-		calcHist(&image, 1, channels, Mat(), hist, 1, histSize, ranges, true, false);*/
-
-		Mat intensityMat = image > 100;
-		return countNonZero(intensityMat) / (double)(image.rows * image.cols);
+		return countNonZero(image > 100) / (double)(image.rows * image.cols);
 	}
 
 	Mat IntensityMeasure::ReadImage(array<byte>^ buffer)
